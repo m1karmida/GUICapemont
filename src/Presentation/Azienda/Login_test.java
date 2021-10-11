f397d2f19fba1c4dfc4764d4dcb289bb87d0b69c
Vincenzo <58863931+Vincenzocarandente@users.noreply.github.com>
2021-10-11 22:35:07 +0200

    Aggiunto test gui per Login (robot)

--- src/Presentation/Azienda/Login_test.java
+++ src/Presentation/Azienda/Login_test.java
@@ -2,21 +2,44 @@ package Presentation.Azienda;
 
 import static org.junit.Assert.*;
 
+import java.awt.Color;
 import java.awt.Robot;
 import java.lang.reflect.Field;
 
-import org.eclipse.swt.events.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import javax.swing.JPasswordField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 
 public class Login_test {
 
 	LoginAzienda la;
+	
+	
 	@Before
 	public void setUp() throws Exception {
-		la=new LoginAzienda();
+
+		la=new LoginAzienda(null);
 		assertNotNull(la);
 	}
 
@@ -26,31 +49,81 @@ public class Login_test {
 		assertNull(la);
 	}
 
+	@SuppressWarnings("deprecation")
 	@Test
-	public void test_click_login() {
-		String stringa ="vincenzo";
+	public void test_click_login() throws InterruptedException {
+		la.setVisible(true);
+		Thread.sleep(1000);
 		Robot robot;
 		try
         {
 			robot = new Robot();
-            boolean upperCase = Character.isUpperCase( stringa.charAt(0) );
-            String variableName = "VK_" + stringa.toUpperCase();
-
-            Class clazz = KeyEvent.class;
-            Field field = clazz.getField( variableName );
-            int keyCode = field.getInt(null);
-
+            robot.mouseMove(900,380);
+            robot.mousePress(InputEvent.BUTTON1_MASK);
             robot.delay(1000);
-
-
-            robot.keyPress( keyCode );
-            robot.keyRelease( keyCode );
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);    
+            
+            robot.keyPress(KeyEvent.VK_A);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_P);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_P);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_L);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_E);
+		    robot.delay(1000);
+		    robot.keyPress(KeyEvent.VK_I);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_C);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_L);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_O);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_U);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_D);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_PERIOD);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_C);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_O);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_M);
+		    robot.delay(200);
+			
+		    robot.mouseMove(900,450);
+		    robot.mousePress(InputEvent.BUTTON1_MASK);
+            robot.delay(1000);
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);    
+            
+            robot.keyPress(KeyEvent.VK_A);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_P);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_P);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_L);
+		    robot.delay(200);
+		    robot.keyPress(KeyEvent.VK_E);
+		    robot.delay(200);
+		    
+		    robot.mouseMove(750,500);
+		    robot.mousePress(InputEvent.BUTTON1_MASK);
+            robot.delay(1000);
+            robot.mouseRelease(InputEvent.BUTTON1_MASK);
+		    
         }
         catch(Exception e)
         {
             System.out.println(e);
         }
 		
+
+	
+		Thread.sleep(1000);
 
 	}
 
